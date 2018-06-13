600fa837a651d52ed84e06603c331c0015cec29b
Kelvin <kelvin-raku@moegi.waseda.jp>
2018-06-13 15:06:25 +0900

    modified:   CNN_def.py

--- CNN_def.py
+++ CNN_def.py
@@ -33,3 +33,60 @@ data=data.flatten()
 # np.savetxt("owl.csv",data,delimiter=",")
 inp = tf.reshape(data,[-1,256,256,1])
 ipdb.set_trace()
+
+def input_fn(is_training, data_dir, batch_size, num_epochs=1, num_gpus=None):
+    
+    dataset = inp
+    
+    return resnet_run_loop.process_record_dataset(
+      dataset=dataset,
+      is_training=is_training,
+      batch_size=batch_size,
+      shuffle_buffer=_SHUFFLE_BUFFER,
+      parse_record_fn=parse_record,
+      num_epochs=num_epochs,
+      num_gpus=num_gpus,
+      examples_per_epoch=_NUM_IMAGES['train'] if is_training else None
+    )
+
+def get_synth_input_fn():
+  return resnet_run_loop.get_synth_input_fn(_HEIGHT, _WIDTH, _NUM_CHANNELS)
+
+class P2SModel(resnet_model.Model):
+  """Model class with appropriate defaults for Imagenet data."""
+
+  def __init__(self, resnet_size, data_format=None, resnet_version=resnet_model.DEFAULT_VERSION, dtype=resnet_model.DEFAULT_DTYPE):
+    """
+    Args:
+      resnet_size: The number of convolutional layers needed in the model.
+      data_format: Either 'channels_first' or 'channels_last', specifying which
+        data format to use when setting up the model.
+      resnet_version: Integer representing which version of the ResNet network
+        to use. See README for details. Valid values: [1, 2]
+      dtype: The TensorFlow dtype to use for calculations.
+    """
+
+    # For bigger models, we want to use "bottleneck" layers
+    if resnet_size < 50:
+      bottleneck = False
+      final_size = 512
+    else:
+      bottleneck = True
+      final_size = 2048
+
+    super(P2SModel, self).__init__(
+        resnet_size=resnet_size,
+        bottleneck=bottleneck,
+        num_classes=num_classes,
+        num_filters=64,
+        kernel_size=7,
+        conv_stride=2,
+        first_pool_size=3,
+        first_pool_stride=2,
+        block_sizes=_get_block_sizes(resnet_size),
+        block_strides=[1, 2, 2, 2],
+        final_size=final_size,
+        resnet_version=resnet_version,
+        data_format=data_format,
+        dtype=dtype
+    )
